70f4aa8ac3a0a626a7916fd2e4039f83afa557d7
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-30 13:37:36 -0400

    Updated from Brython Server: September 30, 2015 1:37:36 PM GMT-4

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: Eric
-Credit: Me!
+Author: Roger Danilek
+Credit: 
 
 Assignment:
 
@@ -25,3 +25,7 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+a= input("Please enter a string of text (the bigger the better): ")
+a.reverse()
+
+
