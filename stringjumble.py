3820650d2d82535d325f2f41820cc56789a60367
RDanilek <rogerdanilek@hanovernorwichschools.org>
2015-09-30 13:37:56 -0400

    Updated from Brython Server: September 30, 2015 1:37:56 PM GMT-4

--- stringjumble.py
+++ stringjumble.py
@@ -26,6 +26,6 @@ handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
 a= input("Please enter a string of text (the bigger the better): ")
-a.reverse()
+print(list(a).reverse())
 
 
